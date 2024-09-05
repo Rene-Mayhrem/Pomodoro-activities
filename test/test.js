04fa135cb888e71e56ce870e8e3d182d4ec5870e
mayhrem <renecruz.0711@gmail.com>
2024-09-05 01:35:50 -0600

    Adding print messages for testing

--- test/test.js
+++ test/test.js
@@ -3,5 +3,11 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log("Hello")
+  res.send('Testing');
+  
+});
+
+const port = 80;
+app.listen(port, () => {
+  console.log(`Server started at ${port}`);
 });
